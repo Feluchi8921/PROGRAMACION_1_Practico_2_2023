f1f45a94029a73b978a32f434f196487ba96c5ed
Felicitas <felicitas.aguerralde@gmail.com>
2023-04-27 20:20:55 -0300

    agrego enunciado ejercicio 2

--- src/Ejercicio_2.java
+++ src/Ejercicio_2.java
@@ -1,3 +1,9 @@
+/* Construir un programa que solicite desde teclado un número de mes y posteriormente notifique
+ por pantalla la cantidad de días de ese mes.
+En el caso de que ingrese 2 como número de mes (febrero) deberá además solicitar ingresar un número de año,
+y dependiendo si es bisiesto o no con la siguiente sentencia:
+((anio % 4 == 0) && ((anio % 100 != 0) || (anio % 400 == 0))) imprimir la cantidad de días correspondiente.*/
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 public class Ejercicio_2 {
