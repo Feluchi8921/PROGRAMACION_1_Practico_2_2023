2d4d6c23cb3a76c8637b5f5039e34308a60da9ce
Felicitas <felicitas.aguerralde@gmail.com>
2023-04-27 20:29:55 -0300

    Agregue enunciado ejercicio 5

--- src/Ejercicio_5.java
+++ src/Ejercicio_5.java
@@ -1,3 +1,20 @@
+/* Escribir un programa que solicite una hora del día (valor entero de la hora solamente) y resuelva en cada caso:
+ Si la hora está entre las 0 y las 5, pida ingresar la temperatura y si la misma es menor a 20 grados imprima
+ “Encender la calefacción”.
+Si es mayor a 25, imprima “Apagar calefacción”.
+Si está en el rango de 20 a 25 imprima “Calefacción encendida, no abra las ventanas!!!”.
+ Si la hora está entre las 6 y las 11, pida un carácter letra minúscula y
+  si es vocal imprima por la consola la cantidad de vocales que tiene la palabra que corresponde con la hora.
+  Ejemplo 8 (ocho) tiene 2 vocales. Si no es vocal imprima la cantidad de consonantes que tiene,
+  para el ejemplo 8 (ocho) tiene 2 consonantes.
+ Si la hora está entre las 12 y 17 y es par, imprima el promedio entre la hora ingresada y el límite inferior del rango.
+  Si la hora es impar debe imprimir el promedio entre la hora ingresada y el límite superior del rango.
+ Si la hora está entre las 18 y 23, pida ingresar una clave numérica, si coincide con la clave almacenada previamente
+  en una constante, pida ingresar una segunda clave de verificación (un valor entre 100 y 999).
+  Para esta segunda clave (que solo la conoce el usuario) se debe verificar que el el dígito de mayor peso (centena)
+   es múltiplo del dígito de mayor peso de la clave almacenada.
+   Si todo esto se cumple debería mostrar por la consola el mensaje Clave correcta.
+*/
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 public class Ejercicio_5 {
