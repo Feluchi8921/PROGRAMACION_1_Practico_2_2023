250ecf1926d49b61a62b62b4a9b3933b0fa3a9c7
Felicitas <felicitas.aguerralde@gmail.com>
2023-04-27 21:22:12 -0300

    Agrego Ejercicio 15

---
+++ src/Ejercicio_15.java
@@ -0,0 +1,29 @@
+/*Pedir por consola el ingreso de números enteros. Cuando se ingrese un 0 se debe terminar el programa informando el
+ promedio de los números ingresados, cuál fue el mayor número y  cuál fue el menor número.
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class Ejercicio_15 {
+    public static void main (String []args){
+        int numero, contador=0;
+        double suma=0, promedio=0;
+        try{
+            BufferedReader entrada= new BufferedReader(new InputStreamReader(System.in));
+            do{
+                System.out.println("Ingrese un numero entero");
+                numero=Integer.valueOf(entrada.readLine());
+                if(numero!=0){
+                    suma+=numero;
+                    contador++;
+                }
+            }
+            while(numero!=0);
+            promedio=(suma/contador);
+            System.out.println("EL promedio de los numero ingresados es: "+promedio);
+        }
+        catch(Exception exc){
+            System.out.println(exc);
+        }
+    }
+}
